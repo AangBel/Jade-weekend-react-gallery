a8736032c6772a40a26438ab474c280c151bf995
Angelica Beltran <angelica.beltran11@gmail.com>
2023-09-16 17:32:39 -0500

    added files and data.js

---
+++ src/components/App/GalleryItem/GalleryItem.jsx
@@ -0,0 +1,12 @@
+import galleryItems from "../../../../server/modules/gallery.data";
+
+export default function plant(){
+    let plantPic = galleryItems.id(5);
+    console.log(plantPic);
+
+    // return (
+    //     <div className="plantClass">
+    //         <
+    //     </div>
+        // )
+}
